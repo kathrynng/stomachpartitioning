02f57353f9f3389df84634f9975788909d86f4e6
Kathryn Ng <ng.kathryn@gmail.com>
2020-07-20 12:07:26 -0700

    added more food

--- stomach-party/src/components/images.js
+++ stomach-party/src/components/images.js
@@ -13,7 +13,23 @@ import CrabLegs from '../images/food_Crab_Legs.png'
 import CurryRice from '../images/food_Curry_Rice.png'
 import EggsBen from '../images/food_Eggs_Benedict.png'
 import FishChips from '../images/food_Fish_and_Chips.png'
+import Salmon from '../images/food_Grilled_Salmon.png'
+import Thali from '../images/food_Indian.png'
+import Kastudon from '../images/food_Katsudon.png'
+import MacCheese from '../images/food_Mixed_Rice.png'
+import Eggplants from '../images/food_Nasu_Nibitashi.png'
+import Sandwich from '../images/food_Sandwich.png'
+import spBolog from '../images/food_Spaghetti_Bolognese.png'
+import spCarbo from '../images/food_Spaghetti_Carbonara.png'
+import Ramen from '../images/food_Shio_Ramen.png'
 
+import Muffin from '../images/sweet_Muffin.png'
+import Crepe from '../images/sweets_Crepe.png'
+import IceCr from '../images/sweets_icecream.png'
+import CandyApple from '../images/sweets_Candy_Apple.png'
+import fruitTart from '../images/sweets_Fruit_Tart.png'
+import honeyToast from '../images/sweets_Honey_Toast.png'
+import milleCrepe from '../images/sweets_Mille_Crepe.png'
 
 export default [
     {"name" : "Apple Pie","source": ApplePie, "type":"sweet"},
@@ -25,5 +41,27 @@ export default [
     {"name" : "BLT Sandwich","source": BLT, "type":"food"},
     {"name" : "Cheese Balls","source": CheeseBall, "type":"food"},
     {"name" : "Chicken","source": Chicken, "type":"food"},
-    {"name" : "Crab Legs","source": CrabLegs, "type":"food"}
+    {"name" : "Crab Legs","source": CrabLegs, "type":"food"},
+    {"name" : "Curry Rice","source": CurryRice, "type":"food"},
+    {"name" : "Eggs Benedict","source": EggsBen, "type":"food"},
+    {"name" : "Fish and Chips","source": FishChips, "type":"food"},
+    {"name" : "Salmon","source": Salmon, "type":"food"},
+    {"name" : "Thali","source": Thali, "type":"food"},
+    {"name" : "Cutlet Bowl","source": Kastudon, "type":"food"},
+    {"name" : "Mac N Cheese","source": MacCheese, "type":"food"},
+    {"name" : "Braised Eggplant","source": Eggplants, "type":"food"},
+    {"name" : "Sandwich","source": Sandwich, "type":"food"},
+    {"name" : "Spaghetti Bolognese","source": spBolog, "type":"food"},
+    {"name" : "Spaghetti Carbonara","source": spCarbo, "type":"food"},
+    {"name" : "Ramen","source": Ramen, "type":"food"},
+
+    {"name" : "Muffin","source": Muffin, "type":"sweets"},
+    {"name" : "Crepe","source": Crepe, "type":"sweets"},
+    {"name" : "Ice Cream","source": IceCr, "type":"sweets"},
+    {"name" : "Candy Apple","source": CandyApple, "type":"sweets"},
+    {"name" : "Fruit Tart","source": fruitTart, "type":"sweets"},
+    {"name" : "Honey Toast","source": honeyToast, "type":"sweets"},
+    {"name" : "Mille Crepe","source": milleCrepe, "type":"sweets"},
+    
+
 ]
