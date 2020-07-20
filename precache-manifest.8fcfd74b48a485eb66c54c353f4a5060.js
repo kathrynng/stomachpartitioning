b0fddc7d2e561d1bc4869ce80fab33683649eb85
Kathryn Ng <ng.kathryn@gmail.com>
2020-07-19 23:49:20 -0700

    Updates

---
+++ precache-manifest.8fcfd74b48a485eb66c54c353f4a5060.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "976c55d200bfffb755f28895937f473a",
+    "url": "/stomachpartitioning/index.html"
+  },
+  {
+    "revision": "9a593478f4093b271f7b",
+    "url": "/stomachpartitioning/static/css/main.031089e6.chunk.css"
+  },
+  {
+    "revision": "ba85a8337c706cb3595e",
+    "url": "/stomachpartitioning/static/js/2.b99fbbee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stomachpartitioning/static/js/2.b99fbbee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a593478f4093b271f7b",
+    "url": "/stomachpartitioning/static/js/main.a0b21c15.chunk.js"
+  },
+  {
+    "revision": "9d912b2262c2a3f4e583",
+    "url": "/stomachpartitioning/static/js/runtime-main.c32ab326.js"
+  },
+  {
+    "revision": "b9863ceb936b80e333d88132f84286cb",
+    "url": "/stomachpartitioning/static/media/computer_harddisk.b9863ceb.png"
+  },
+  {
+    "revision": "87a816a3a365b786ed3bf5b806f6bf6e",
+    "url": "/stomachpartitioning/static/media/food_BLT_Sandwich.87a816a3.png"
+  },
+  {
+    "revision": "4e2c8a7e4673c4714e642d3187947432",
+    "url": "/stomachpartitioning/static/media/food_Banh_Mi_Sandwich.4e2c8a7e.png"
+  },
+  {
+    "revision": "562af3a022092d04d67aafbab5f8faf8",
+    "url": "/stomachpartitioning/static/media/food_Cheese_Ball.562af3a0.png"
+  },
+  {
+    "revision": "cb99e2d27fac86c0f699e41343d0907b",
+    "url": "/stomachpartitioning/static/media/food_Chicken.cb99e2d2.png"
+  },
+  {
+    "revision": "762cd76038b97fc1ed396bec9e8b5e9f",
+    "url": "/stomachpartitioning/static/media/food_Crab_Legs.762cd760.png"
+  },
+  {
+    "revision": "658120331ffb71eb57ef927d2f59bbd0",
+    "url": "/stomachpartitioning/static/media/food_Curry_Rice.65812033.png"
+  },
+  {
+    "revision": "6bf876c35895db7e9ccc9cb611cd5607",
+    "url": "/stomachpartitioning/static/media/food_Eggs_Benedict.6bf876c3.png"
+  },
+  {
+    "revision": "c250b35de2dc69ee0fdda6d812504e3b",
+    "url": "/stomachpartitioning/static/media/food_Fish_and_Chips.c250b35d.png"
+  },
+  {
+    "revision": "a393fe8a0e8bc672f3f5f4a4198d6bd0",
+    "url": "/stomachpartitioning/static/media/food_Grilled_Salmon.a393fe8a.png"
+  },
+  {
+    "revision": "9cc7b6e0e8437457d947e713379e8139",
+    "url": "/stomachpartitioning/static/media/food_Lasagna.9cc7b6e0.png"
+  },
+  {
+    "revision": "59b0341d5770d1c6d157abd669a2d2dc",
+    "url": "/stomachpartitioning/static/media/food_Mixed_Nuts.59b0341d.png"
+  },
+  {
+    "revision": "d66cb70df8be5989d8e2117719fdc8eb",
+    "url": "/stomachpartitioning/static/media/food_Pretzel.d66cb70d.png"
+  },
+  {
+    "revision": "1920b9783842fef92beca3a2b101481b",
+    "url": "/stomachpartitioning/static/media/food_Shio_Ramen.1920b978.png"
+  },
+  {
+    "revision": "e8567c771f94fadf0bdf814279ee4dde",
+    "url": "/stomachpartitioning/static/media/stomach.e8567c77.png"
+  },
+  {
+    "revision": "cae53ed0633efd39629432f5af732cab",
+    "url": "/stomachpartitioning/static/media/sweet_Muffin.cae53ed0.png"
+  },
+  {
+    "revision": "03472f9fb4a672f0c996a168baf89f80",
+    "url": "/stomachpartitioning/static/media/sweets_Apple_Pie.03472f9f.png"
+  },
+  {
+    "revision": "07bf336832c50ccb70db48668492099b",
+    "url": "/stomachpartitioning/static/media/sweets_Crepe.07bf3368.png"
+  },
+  {
+    "revision": "31213bb0bc222061bd209076a5d3fb62",
+    "url": "/stomachpartitioning/static/media/sweets_icecream.31213bb0.png"
+  },
+  {
+    "revision": "dfe093e7b40ed421704fbde3796fb7f7",
+    "url": "/stomachpartitioning/static/media/sweets_jelly.dfe093e7.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e9c66fed7c1cc864984ad0c49f27db40.js"
+  "/stomachpartitioning/precache-manifest.8fcfd74b48a485eb66c54c353f4a5060.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/stomachpartitioning/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
